9292169ebdb7492fd981db332c5679363a4f2240
Pai, Kiran <Kiran.Pai@emc.com>
2020-09-30 03:14:40 +0530

    Completed the signin controller for the backend

--- projbackend/controllers/auth.js
+++ projbackend/controllers/auth.js
@@ -1,6 +1,19 @@
 const User = require('../models/user');
+const { validationResult } = require('express-validator');
+var jwt = require('jsonwebtoken');
+var expressJwt = require('express-jwt');
 
 exports.signup = (req,res) => {
+    const errors = validationResult(req);
+
+    // Refer express validator documantation    
+    if(!errors.isEmpty()){
+        return res.status(422).json({
+            error : errors.array()[0].msg,
+            param : errors.array()[0].param
+        });
+    }
+
     const user = new User(req.body);
     user.save((err, user) => {
         if(err){
@@ -14,6 +27,44 @@ exports.signup = (req,res) => {
             id : user._id
         });
     });
+}
+
+exports.signin = (req,res) => {
+    const {email, password} = req.body;
+    const errors = validationResult(req);
+   
+    if(!errors.isEmpty()){
+        return res.status(422).json({
+            error : errors.array()[0].msg,
+            param : errors.array()[0].param
+        });
+    }
+
+    User.findOne({email}, (err, user) => {
+        if(err){
+            return res.status(401).json({
+                error : "USER email does not exists"
+            })
+        }
+
+        if(!user.authenticate(password)){
+            return res.status(400).json({
+                error : "Email and password do not match"
+            })
+        }
+        
+        // create token
+        const token = jwt.sign({_id : user._id}, process.env.SECRET);
+        // put token in cookie
+        res.cookie("token", token, {expire: new Date() + 9999});
+
+        // send response to front end
+        const {_id, name, email, role} = user;
+        return res.json({token, user: {_id, name, email, role}});
+
+    })
+
+
 }
 
 
--- projbackend/routes/auth.js
+++ projbackend/routes/auth.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
+const { check } = require('express-validator');
 
-const {signout, signup} = require('../controllers/auth')
+const {signout, signup, signin} = require('../controllers/auth')
 
-router.post('/signup', signup)
+router.post('/signup', [
+    check("name", "Name should have a minimum length of 3").isLength({ min:3 }),
+    check("email", "Please enter a valid email").isEmail(),
+    check("password", "Password should have a minimum length of 3").isLength({ min:3 }),
+], signup)
+
+router.post('/signin', [
+    check("email", "Email is required").isEmail(),
+    check("password", "Password field is required").isLength({ min:3 }),
+], signin)
 
 router.get('/signout', signout);
 
