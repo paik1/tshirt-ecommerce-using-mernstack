7a39e03d734568b7162a38520515e9bb3055da13
Pai, Kiran <kiran.pai95@gmail.com>
2020-10-10 18:30:08 +0530

    category changes

--- projbackend/controllers/category.js
+++ projbackend/controllers/category.js
@@ -16,5 +16,42 @@ exports.getCategoryById = (req, res, next, id) => {
 }
 
 exports.createCategory = (req,res) => {
-    const category
+    const category = new Category(req.body);
+    category.save((err,category) => {
+        if(err){
+            return res.status(400).json({
+                error : "NOT able to save category in DB"
+            });
+        }
+        res.json(category); 
+    });
+}
+
+exports.getCategory = (req, res) => {
+    return res.json(req.category);
+}
+
+exports.getAllCategory = (req,res) => {
+    Category.find().exec((err, categories) => {
+        if(err){
+            return res.status(400).json({
+                error : "No categories found"
+            });
+        }
+        res.json(categories);
+    });
+}
+
+exports.updateCategory = (req, res) => {
+    const category = req.category;
+    category.name = req.body.name;
+
+    category.save((err, updatedCategory) => {
+        if(err){
+            return res.status(400).json({
+                error : "Could not update the category"
+            });
+        }
+        res.json(updatedCategory);
+    });
 }
